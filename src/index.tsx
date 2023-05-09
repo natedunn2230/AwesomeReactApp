7bfd50233336fb491000f930517fe698fc4d4fe4
XarosElite <polingj999@gmail.com>
2023-05-08 20:40:00 -0400

    Attempt at a MUI nav bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from "react-dom/client";
 import Hello from "./Hello";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Hello />);
+root.render(
+    <>
+    <ResponsiveAppBar />
+    <Hello />
+    </>
+    
+
+);
